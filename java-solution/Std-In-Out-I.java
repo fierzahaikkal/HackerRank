fa87d0276026b0dfd3cc75bb3402b70dce36088d
itsMookbac <83209105+mookbac@users.noreply.github.com>
2022-02-19 11:26:25 +0700

    menambahkan Java Std In and Out I

---
+++ java-solution/Std-In-Out-I.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner userInput = new Scanner(System.in);
+        int myInt = userInput.nextInt();
+        int myInt2 = userInput.nextInt();
+        int myInt3 = userInput.nextInt();
+        
+        System.out.println(myInt);
+        System.out.println(myInt2);
+        System.out.println(myInt3);
+    }
+}
